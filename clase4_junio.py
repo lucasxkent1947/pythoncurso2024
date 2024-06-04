8235c046b4ef6f91fbb873703a0383c71f4b6f30
Lucas Rodriguez <Lucas Nicolas Rodriguez>
2024-06-04 20:43:20 -0300

    Primera Subida a github del curso

---
+++ clase4_junio.py
@@ -0,0 +1,17 @@
+
+#Podés comentar el codigo, con numeral "#" delante para una sola linea
+"""
+O podes utilizar las triples comillas para comentar multilinea
+como
+en 
+este 
+ejemplo
+
+"""
+print(1+23)
+
+
+#ASDSAD
+#SADASDASD
+#DSFSD
+print(654-6)
